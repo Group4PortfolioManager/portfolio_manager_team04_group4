31059cc717afc53d68fdf6530dea49e343d4a007
Ihsane03 <ihsane.majdoubi@polymtl.ca>
2026-07-27 14:27:36 -0400

    Mocked return values for test

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ from app.routes.api_routes import api_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    return {'message': 'Welcome to the Portfolio Manager API'}
+
 app.register_blueprint(api_bp)
 
 
