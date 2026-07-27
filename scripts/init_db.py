1d12b6cbcba01a1cd0ffdb76bbec00644111270b
Yvehenry <julsain.ys@gmail.com>
2026-07-27 19:52:57 +0000

    finished the code that creates the database and started the code to fetch the information for the landing page

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,19 +1,38 @@
-# File used to connect to the database
+"""
+File used to connect to the database
+"""
+
 import mysql.connector
+import os
+from dotenv import load_dotev
 
-# Connect to MySQL Server
-db = mysql.connector.connect(
-    host="",
-    user="",
-    passwrd=""
-)
+def start_database():
+    # Load environment variables
+    load_dotev()
 
-# 
-c = db.cursor()
+    # Connect to MySQL Server
+    db = mysql.connector.connect(
+        host=os.getenv("db_host"),
+        user=os.getenv("db_user"),
+        password=os.getenv("db_password")
+        #database=os.getenv("db_name"), # assume that database might not be created
+    )
 
-db_name = ""
+    # Get the name of the database to be created
+    db_name = os.getenv("db_name")
 
-# Creates the database in the server if it does not exist
-c.execute("IF DB_ID(%s) IS NULL BEGIN CREATE DATABASE %s END", db_name, db_name)
+    # Checks if database already exists in the system
+    cursor = db.cursor()
+    #cursor.execute("CREATE DATABASE IF NOT EXIST %s", db_name)
+    cursor.execute("SHOW DATABASES LIKE '%s';", db_name)
+
+    # Creates the database in the server if it does not exist
+    if cursor.rowcount <= 0:    
+        with open("schema.sql") as file:
+            script = file.read()
+        cursor.execute(script, multi=True)
+        results = cursor.fetchall()
+        for line in results:
+            print(line)
 
 
