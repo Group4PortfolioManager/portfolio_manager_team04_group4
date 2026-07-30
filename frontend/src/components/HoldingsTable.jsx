f7006041d750160920b747017c39e16e81669eb2
nwooster34 <natewooster34@gmail.com>
2026-07-30 18:59:01 +0000

    Updated button to View Holdings and deleted SQL Schema code from database.md

--- frontend/src/components/HoldingsTable.jsx
+++ frontend/src/components/HoldingsTable.jsx
@@ -31,7 +31,7 @@ function HoldingsTable({ showLink = false }) {
       </table>
 
       {showLink && (
-        <Link to="/holdings" className="text-link">Add or Remove Holdings</Link>
+        <Link to="/holdings" className="text-link">View Holdings</Link>
       )}
     </div>
   );
